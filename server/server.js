0efa3d4f672fce878ab925ae611ad340137d4e47
Calvin Yee <calvinyee@Calvins-MacBook-Pro.local>
2025-11-04 20:30:20 -0800

    format file with correct spacing

--- server/server.js
+++ server/server.js
@@ -4,63 +4,55 @@ import PlayerController from './controllers/player.controller.js';
 
 const playerController = new PlayerController();
 const server = new WebSocketServer({ 
-    port: 8081 
+  port: 8081 
 });
 const clients = new Set();
 
 const handleMessage = (message, socket) => {
-    const splitRoute = message.route.split('/');
-    const route = splitRoute[0];
-    let response = message;
-    switch (route) {
-        case CONSTANTS.API_ROUTES.ADMIN.ROUTE:
-            sendToClients(message);
-            break;
-        case CONSTANTS.API_ROUTES.PLAYER.ROUTE:
-            response = playerController.getResponse(response);
-            break;
-    }
-    sendToClients(response);
+  const splitRoute = message.route.split('/');
+  const route = splitRoute[0];
+  let response = message;
+  switch (route) {
+    case CONSTANTS.API_ROUTES.ADMIN.ROUTE:
+      sendToClients(message);
+      break;
+    case CONSTANTS.API_ROUTES.PLAYER.ROUTE:
+      response = playerController.getResponse(response);
+      break;
+  }
+  sendToClients(response);
 }
 
 const sendToClients = (response) => {
-    clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(response));
-        }
-    });
-}
-
-const addClient = (socket) => {
-    clients.add(socket);
-}
-
-const deleteClient = (socket) => {
-    clients.delete(socket);
+  clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(response));
+    }
+  });
 }
 
 server.on('connection', (socket) => {
-    addClient(socket);
+  clients.add(socket);
 
-    socket.on('message', (message) => {
-        let parsedMsg = null;
-        try {
-            parsedMsg = JSON.parse(Buffer.from(message).toString('utf8'));
-        } catch (e) {
-            console.error(e);
-        }
-        if (parsedMsg) {
-            handleMessage(parsedMsg, socket);
-        }
-    });
+  socket.on('message', (message) => {
+    let parsedMsg = null;
+    try {
+      parsedMsg = JSON.parse(Buffer.from(message).toString('utf8'));
+    } catch (e) {
+      console.error(e);
+    }
+    if (parsedMsg) {
+      handleMessage(parsedMsg, socket);
+    }
+  });
 
-    socket.on('close', () => {
-        deleteClient(socket);
-    });
+  socket.on('close', () => {
+    clients.delete(socket);
+  });
 
-    socket.on('error', (error) => {
-        console.error(`Socket error: ${error.message}`);
-    });
+  socket.on('error', (error) => {
+    console.error(`Socket error: ${error.message}`);
+  });
 });
 
 console.log('Server running on Port: ' + 8081);
